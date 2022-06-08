dc9a0fc606d9deb61cb414340eabee1873ddef41
Seiya Kawamoto <kid960805@gmail.com>
2022-06-08 10:27:38 +0900

    fix: Layout/IndentationWidth and Layout/IndentationConsistency

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
 
   private
 
-    def task_params
-      params.require(:task).permit(:name, :content)
-    end
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
 end
